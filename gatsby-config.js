fa05857c5acbfbec040a5db474f845f536a2391d
mock4ever <63812192+mock4ever@users.noreply.github.com>
2020-04-16 18:07:42 -0300

    feat(blog): gatsby basic blog

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Mock4Ever`,
+    author: `Mockers`,
+    description: `Descrição a ser definida`,
+    social: [
+      {
+        name: `mock4ever`,
+        url: `https://github.com/mock4ever`,
+      },
+    ],
+  },
+}
