7524d3d59684369c98247d3dc33ac06a55e87210
Evandro <evandrosimendes@gmail.com>
2020-04-20 12:58:47 -0300

    Adicionando post Yagni

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -1,14 +1,8 @@
 import React, { Fragment } from "react"
 import { Styled } from "theme-ui"
 
-/**
- * Shadow me to add your own bio content
- */
-
 export default () => (
   <Fragment>
-    Words by <Styled.a href="http://example.com/">Jane Doe</Styled.a>.
-    <br />
-    Change me. This is all quite default.
+    Escrito por <Styled.a href="#">mock4ever</Styled.a>.
   </Fragment>
 )
